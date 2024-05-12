e22423cc82efcaf0260498caf0e61ef987ef8bfd
durpintm <durpinthapa@gmail.com>
2024-05-12 01:23:24 -0400

    Zod intro

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const zod = require("zod");
 
 const port = process.env.PORT | 3000;
 
 const app = express();
 app.use(bodyParser.json());
+// app.use(express.json());
 
-app.post("/", function (req, res) {
-  const message = req.query.message;
-  // console.log(req.body);
+// const schema = zod.array(zod.number);
 
-  console.log(message);
-  res.send("Hello World");
+const schema = zod.object({
+  email: zod.string(),
+  password: zod.string(),
+  country: zod.literal("IN").or(zod.literal("US")),
+  kidneys: zod.array(zod.number),
 });
 
-app.get("/hello", function (req, res) {
-  res.json({
-    name: "Durpin",
-    age: 21,
-  });
+app.post("/health-checkup", function (req, res) {
+  const kidneys = req.body.kidneys;
+  const response = schema.safeParse(kidneys);
+  if (!response.success) {
+    res.status(411).json({
+      msg: "Invalid input",
+    });
+  } else {
+    res.send({ response });
+  }
 });
 
 app.listen(port, () => {
